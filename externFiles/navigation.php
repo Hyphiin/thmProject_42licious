f85d535d32fdaf202826cf95bf29b5d60fccc88d
JoeyCroner <lauramaurer64@web.de>
2020-05-05 16:44:02 +0200

    V 0.1

---
+++ externFiles/navigation.php
@@ -0,0 +1,18 @@
+<div class="navbar-links" xmlns="">
+    <a href="index.php">
+        <img alt="Logo-Links-Navbar" class="logo" src="images/42licious_bearbeitet.png">
+    </a>
+
+    <a href="#" class="img-profile">
+        <img alt="Profil-Rechts-Navbar" src="images/profil/flo.png">
+    </a>
+
+    <ul class="content" id="b">
+        <li><a href="#">Kochbuch</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Profil</a></li>
+    </ul>
+
+
+
+</div>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>42licious</title>
+    <link href="css/general.css" rel="stylesheet" type="text/css">
+    <link href="css/navigation.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+
+
+</head>
+<body>
+
+
+<div id="website">
+
+    <?php include("externFiles/Navigation.php"); ?>
+
+    <div id="main" class="main">
+
+        <div class="container">
+            <h1>Top der Woche</h1>
+
+            <div class="recipe-preview">
+                    <diV class="recipe-preview-image-container">
+                        <img src="images/lasagne.jpg" alt="">
+                    </diV>
+                    <div class="recipe-preview-description">
+                        <h4>Rezeptname</h4>
+                        <h5>von Username</h5>
+                    </div>
+            </div>
+
+            <div class="recipe-preview">
+                <diV class="recipe-preview-image-container">
+                    <img src="images/lasagne.jpg" alt="">
+                </diV>
+                <div class="recipe-preview-description">
+                    <h4>Rezeptname</h4>
+                    <h5>von Username</h5>
+                </div>
+            </div>
+
+            <div class="recipe-preview">
+                <diV class="recipe-preview-image-container">
+                    <img src="images/lasagne.jpg" alt="">
+                </diV>
+                <div class="recipe-preview-description">
+                    <h4>Rezeptname</h4>
+                    <h5>von Username</h5>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container">
+            <h1>Vorschläge für Dich</h1>
+
+            <div class="recipe-preview">
+                <diV class="recipe-preview-image-container">
+                    <img src="images/lasagne.jpg" alt="">
+                </diV>
+                <div class="recipe-preview-description">
+                    <h4>Rezeptname</h4>
+                    <h5>von Username</h5>
+                </div>
+            </div>
+
+            <div class="recipe-preview">
+                <diV class="recipe-preview-image-container">
+                    <img src="images/lasagne.jpg" alt="">
+                </diV>
+                <div class="recipe-preview-description">
+                    <h4>Rezeptname</h4>
+                    <h5>von Username</h5>
+                </div>
+            </div>
+
+            <div class="recipe-preview">
+                <diV class="recipe-preview-image-container">
+                    <img src="images/lasagne.jpg" alt="">
+                </diV>
+                <div class="recipe-preview-description">
+                    <h4>Rezeptname</h4>
+                    <h5>von Username</h5>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+</body>
+</html>
