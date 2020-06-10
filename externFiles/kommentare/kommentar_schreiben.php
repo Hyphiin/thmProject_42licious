f6e76a9f89c5600347dc34572ba62edaf38c20c2
Felix Wegener <felix.wegener@iem.thm.de>
2020-06-10 20:15:56 +0200

    Kommentar hinzugefügt

---
+++ externFiles/kommentare/kommentar_schreiben.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>42licious-Kommentare-Ansicht</title>
+    <link href="../../css/general.css" rel="stylesheet" type="text/css">
+    <link href="../../css/kommentare_css/kommentar_schreiben.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="website">
+    <div id="main">
+        <div id="top-buttons">
+
+            <button class="button">Zurück</button>
+
+        </div>
+        <div id="main-content">
+
+            <header id="kommentare-head">
+                <h1>Kommentare</h1>
+            </header>
+
+            <div id="kommentar_schreiben">
+                <textarea id="kommentieren" cols="50" rows="4" placeholder="Kommentar schreiben..."></textarea>
+
+                <span id="commentbuttons">
+                <button id="abbrechen"> Abbrechen </button>
+
+                <button id="abschicken" type="submit"> Kommentieren </button>
+                </span>
+
+            </div>
+
+            <div id="kommentar_sektion">
+                <ul>
+                    <li>
+                        <textarea placeholder="Kommentar"></textarea>
+                    </li>
+                    <li>
+                        <textarea placeholder="Kommentar"></textarea>
+                    </li>
+                    <li>
+                        <textarea placeholder="Kommentar"></textarea>
+                    </li>
+                </ul>
+            </div>
+
+            <button id="mehr_Anzeigen">mehr Anzeigen</button>
+
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ externFiles/kommentare/kommentare_ansicht.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>42licious-Kommentare-Ansicht</title>
+    <link href="../../css/general.css" rel="stylesheet" type="text/css">
+    <link href="../../css/kommentare_css/kommentare_ansicht.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="website">
+    <div id="main">
+        <div id="top-buttons">
+
+            <button class="button">Zurück</button>
+
+        </div>
+        <div id="main-content">
+
+            <header id="kommentare-head">
+                <h1>Kommentare</h1>
+                <button id="kommentar_schreiben">Kommentar schreiben</button>
+            </header>
+
+            <div id="kommentar_sektion">
+            <ul>
+                <li>
+                    <textarea placeholder="Kommentar"></textarea>
+                </li>
+                <li>
+                    <textarea placeholder="Kommentar"></textarea>
+                </li>
+                <li>
+                    <textarea placeholder="Kommentar"></textarea>
+                </li>
+            </ul>
+            </div>
+
+            <button id="mehr_Anzeigen">mehr Anzeigen</button>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
