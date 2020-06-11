50aa26b430b6d3c0e859947d10732df2b0a241a6
Felix Wegener <felix.wegener@iem.thm.de>
2020-06-11 17:06:23 +0200

    weitere Verlinkungen

--- externFiles/profil/profil_rezept.php
+++ externFiles/profil/profil_rezept.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <title>42licious-Profil-Rezeptansicht</title>
     <link href="../../css/general.css" rel="stylesheet" type="text/css">
@@ -13,7 +13,120 @@
 <div id="website">
     <div id="main">
 
+        <div id="top-buttons">
+
+            <a href="profil_ansicht.php"><button class="button">Zurück</button></a>
+
+        </div>
+
+        <div id="main-content">
+            <div id="profil_rezept-title">
+                <h1>Rezepte von USER</h1>
+            </div>
+
+            <label for="sortieren">Sortieren nach</label>
+            <select id="sortieren" size="1">
+                <option>Bewertung</option>
+                <option>Name</option>
+                <option>Erstellungsdatum</option>
+            </select>
+
+            <div id="rezepte">
+                <div id="rezept">
+                    <div id="rezeptinfos">
+                        <img src="../../images/lasagne.jpg">
+                        <div id="infos">
+                            <p id="rezepttitle">Rezeptname</p>
+                            <p id="User">von USER</p>
+                            <div id="bewertung">Bewertung:</div>
+                            <div id="dauer">Dauer: 50min</div>
+                            <div id="schwierigkeit">Schwierigkeit: mittel</div>
+                        </div>
+                    </div>
+
+                    <div id="beschreibung">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque dignissim
+                            ipsum id placerat.
+                            Aenean egestas lacus et quam varius, sit amet hendrerit lorem rutrum. Morbi feugiat velit
+                            quis pretium auctor.
+                            Praesent a magna vel mauris interdum bibendum et sit amet augue. Aliquam blandit, diam nec
+                            elementum vehicula,
+                            lorem sem mollis libero, at pellentesque nunc nibh et lorem. Cras ac facilisis quam. Donec
+                            laoreet nisi at purus venenatis,
+                            a auctor tellus mattis. Aliquam erat volutpat. Pellentesque ac neque ac dui tincidunt
+                            fringilla. Duis in elementum felis,
+                            sit amet porttitor ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum
+                            et malesuada fames ac ante
+                            ipsum primis in faucibus.
+                        </p>
+                    </div>
+                </div>
+                <div id="rezept">
+                    <div id="rezeptinfos">
+                        <img src="../../images/lasagne.jpg">
+                        <div id="infos">
+                            <p id="rezepttitle">Rezeptname</p>
+                            <p id="User">von USER</p>
+                            <div id="bewertung">Bewertung:</div>
+                            <div id="dauer">Dauer: 50min</div>
+                            <div id="schwierigkeit">Schwierigkeit: mittel</div>
+                        </div>
+                    </div>
+                    <div id="beschreibung">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque dignissim
+                            ipsum id placerat.
+                            Aenean egestas lacus et quam varius, sit amet hendrerit lorem rutrum. Morbi feugiat velit
+                            quis pretium auctor.
+                            Praesent a magna vel mauris interdum bibendum et sit amet augue. Aliquam blandit, diam nec
+                            elementum vehicula,
+                            lorem sem mollis libero, at pellentesque nunc nibh et lorem. Cras ac facilisis quam. Donec
+                            laoreet nisi at purus venenatis,
+                            a auctor tellus mattis. Aliquam erat volutpat. Pellentesque ac neque ac dui tincidunt
+                            fringilla. Duis in elementum felis,
+                            sit amet porttitor ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum
+                            et malesuada fames ac ante
+                            ipsum primis in faucibus.
+                        </p>
+                    </div>
+                </div>
+                <div id="rezept">
+                    <div id="rezeptinfos">
+                        <img src="../../images/lasagne.jpg">
+                        <div id="infos">
+                            <p id="rezepttitle">Rezeptname</p>
+                            <p id="User">von USER</p>
+                            <div id="bewertung">Bewertung:</div>
+                            <div id="dauer">Dauer: 50min</div>
+                            <div id="schwierigkeit">Schwierigkeit: mittel</div>
+                        </div>
+                    </div>
+                    <div id="beschreibung">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque dignissim
+                            ipsum id placerat.
+                            Aenean egestas lacus et quam varius, sit amet hendrerit lorem rutrum. Morbi feugiat velit
+                            quis pretium auctor.
+                            Praesent a magna vel mauris interdum bibendum et sit amet augue. Aliquam blandit, diam nec
+                            elementum vehicula,
+                            lorem sem mollis libero, at pellentesque nunc nibh et lorem. Cras ac facilisis quam. Donec
+                            laoreet nisi at purus venenatis,
+                            a auctor tellus mattis. Aliquam erat volutpat. Pellentesque ac neque ac dui tincidunt
+                            fringilla. Duis in elementum felis,
+                            sit amet porttitor ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum
+                            et malesuada fames ac ante
+                            ipsum primis in faucibus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="bottom-buttons">
+                <button class="button" id="show-more">Mehr anzeigen</button>
+            </div>
+
+        </div>
     </div>
-</div>
 </body>
 </html>
