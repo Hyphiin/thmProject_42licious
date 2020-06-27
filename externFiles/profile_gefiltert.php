351d3b2f5c78abbe4915c03f404bfab7aafe6304
Marcel <marcel.sippel@iem.thm.de>
2020-06-27 17:13:07 +0200

    Profile_gefiltert formatiert

--- externFiles/profile_gefiltert.php
+++ externFiles/profile_gefiltert.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=42licious', 'root', '');
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$sess = $_SESSION['userid'];
+
+if ($sess == true) {
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -7,8 +17,8 @@
     <title>42licious-Profil-Ansicht</title>
     <link href="../css/general.css" rel="stylesheet" type="text/css">
     <link href="../css/navigation.css" rel="stylesheet" type="text/css">
-    <link href="../css/main.css" rel="stylesheet" type="text/css">
     <link href="../css/profil_css/profil_ansicht.css" rel="stylesheet" type="text/css">
+    <link href="../css/profil_css/profil_anzeigen.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
@@ -16,12 +26,36 @@
 
     <?php include("navigation.php"); ?>
 
+    <div id="main">
+
+        <div class="main-content">
+
+            <div class="profil-list">
+
+                <?php
+
+                if(isset($_GET["suchbegriff"])) {
+                    $suchwort = $_GET["suchbegriff"];
+
+                echo '<div id="head-title">';
+                    echo    '<h1>Suchergebnisse für "'.$suchwort.'"</h1>';
+                    echo' </div>';
+
+                    echo'<div id="top-buttons">';
+                    echo   '<div>';
+                    echo    '<label for="filter">Sortieren nach:</label>';
+                    echo   '<select id="filter" name="filter">';
+                    echo       '<option value="name">Name</option>';
+                    echo       '<option value="date">Datum</option>';
+                    echo    '</select>';
+                    echo '</div>';
+
+                    echo '</div>';
+
+                    echo '<div class="users">';
 
 
-    <?php
 
-    if(isset($_GET["suchbegriff"])) {
-        $suchwort = $_GET["suchbegriff"];
         $suchwort = explode(" ", $suchwort);
         $abfrage = "";
         $a = array('vorname', 'nachname', 'nickname');
@@ -59,17 +93,21 @@
             if(is_object($ergebnis)){
                 while($zeile = $ergebnis->fetch_object())
                 {
-                    echo '<div id="users">';
-                    echo $zeile->vorname;
-                    echo '<br>';
-                    echo $zeile->nachname;
-                    echo '<br>';
-                    echo $zeile->nickname;
-                    echo '<br>';
-                    echo '<br>';
-                    echo '<br>';
-                    echo '</div>';
-
+                    echo    '<a href="profil_ansicht.php?id='.$zeile->id.'">';
+                    echo        '<div class="profil-preview">';
+                    echo        '<div class="profil-preview-body">';
+                    echo            '<div class="profil-preview-pic">';
+                    echo               '<img alt="Profil-Bild" id="profil_bild" src='."$zeile->pic".'>';
+                    echo            '</div>';
+                    echo            '<div class="profil-preview-info">';
+                    echo                '<p>Name: '.$zeile->vorname." ".substr($zeile->nachname,0,1).".".'</p>';
+                    echo                '<p>Nutzername: '.$zeile->nickname.'</p>';
+                    echo                   '<br>';
+                    echo                '<p>Mitglied seit: '.$zeile->created_at.'</p>';
+                    echo            '</div>';
+                    echo            '</div>';
+                    echo        '</div>';
+                    echo    '</a>';
                 }
             }
         }
@@ -77,7 +115,33 @@
     }
 
     ?>
+                </div>
+                <div id="bottom-buttons">
+                    <button class="button" id="show-more">Mehr anzeigen</button>
+                </div>
 
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="../jscript/profilPreview.js"></script>
 
 </body>
 </html>
+    <?php
+} else if($sess != true){
+    echo '<div id="website">';
+
+    echo'<div id="main">';
+    echo'<div id="main-content">';
+    echo"Bitte einloggen!". " ". '<a href="login.php">zum Login</a>';
+    echo'<br>';
+    echo"Noch kein Mitglied?". " ". '<a href="registrieren.php">Mitglied werden!</a>';
+    echo'</div>';
+    echo'</div>';
+    echo'</div>';
+}
+?>
