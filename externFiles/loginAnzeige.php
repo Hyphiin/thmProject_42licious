2b0185ee4e5ccea4e36046c9b520ea249bdaddb6
Chiara <ck@rswz.de>
2020-06-29 14:00:54 +0200

    Login+Logout hübschere Seiten

---
+++ externFiles/loginAnzeige.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <title>NoSess</title>
+    <link href="../css/general.css" rel="stylesheet" type="text/css">
+    <link href="../css/navigation.css" rel="stylesheet" type="text/css">
+    <link href="../css/main.css" rel="stylesheet" type="text/css">
+    <link href="../css/nosess.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="website">
+    <?php include("navigation.php"); ?>
+
+    <div id="main">
+        <div id="main-content">
+            <?php
+            echo 'Login erfolgreich!';
+            echo '<br><br>';
+            echo '<a href="index.php"><button class="button" id="back">Weiter zur Startseite</button></a>';
+            echo '<br><br>';
+            ?>
+        </div>
+    </div>
+
+</div>
+</body>
+
+</html>
--- externFiles/logout.php
+++ externFiles/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
     session_destroy();
-    echo "Logout erfolgreich";
+    die(include 'logoutAnzeige.php');
 
 ?>
 
