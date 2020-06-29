10d01fcd986443475af4d8ed4308fbbd73c4978e
Marcel <marcel.sippel@iem.thm.de>
2020-06-29 12:52:06 +0200

    RezeptErstellen V1

--- externFiles/rezeptErstellen.php
+++ externFiles/rezeptErstellen.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=42licious', 'root', '');
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$sess = $_SESSION['userid'];
+
+if ($sess == true) {
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -14,30 +24,97 @@
 
 <div id="website">
 
-    <?php include("navigation.php"); ?>
+    <?php include("navigation.php");
+
+
+    if(isset($_GET['erstellen'])) {
+        $titel = $_POST['titel'];
+        $pic = $_POST['pic'];
+        $dauer = $_POST['dauer'];
+        $schwierigkeit = $_POST['schwierigkeit'];
+        $beschreibung = $_POST['beschreibung'];
+        $anleitung = $_POST['anleitung'];
+        $cat1 = $_POST['cat1'];
+        $cat2 = $_POST['cat2'];
+        $cat3 = $_POST['cat3'];
+        $kategorien = "";
+
+        if($cat1!=""){
+            $kategorien.=$cat1.";";
+        }
+        if($cat2!=""){
+            $kategorien.=$cat2.";";
+        }
+        if($cat3!=""){
+            $kategorien.=$cat3.";";
+        }
+
+        $counter = $_POST['tableLength'];
+        $zutatenListe = "";
+
+        for($i = 0; $i < ($counter+1); $i+=1){
+            $menge = $_POST['menge'.$i.''];
+            $zutat = $_POST['zutaten'.$i.''];
+
+            if($menge!="" || $zutat!=""){
+                $zutatenListe .= $menge . ":" . $zutat . ";";
+            }
+
+        }
+
+        $statement = $pdo->prepare("INSERT INTO rezepte (uid, titel, dauer, schwierigkeit, kategorien, beschreibung, zutatenListe,  anleitung, pic) VALUES (:uid, :titel , :dauer, :schwierigkeit, :kategorien, :beschreibung, :zutatenListe, :anleitung, :pic)");
+        $result = $statement->execute(array('uid' => $sess, 'titel' => $titel, 'dauer' => $dauer, 'schwierigkeit' => $schwierigkeit,'kategorien' => $kategorien, 'beschreibung' => $beschreibung, 'zutatenListe' => $zutatenListe, 'anleitung' => $anleitung, 'pic' => $pic));
+    }
+    ?>
 
     <div id="main">
 
         <div id="create-recipe">
             <h1>Rezept erstellen</h1>
 
-            <form id="recipe-erstellen" action="?hochladen" method="post">
+            <form id="recipe-erstellen" action="?erstellen" method="post">
+
                 <label for="titel">Titel:</label>
                 <input type="text" name="titel" id="titel" size="40" placeholder="Titel eingeben..."><br/>
                 <br/>
+
+                Vorschaubild hochladen:<br>
+                <input type="file" accept="image/*" name="pic"/><br><br>
+
                 <label for="dauer">Dauer:</label>
-                <input type="text" name="dauer" id="dauer" size="40" placeholder="Minuten eingeben..."><br/>
+                <select name="dauer" id="dauer">
+                    <option value="15 Min">15 Min</option>
+                    <option value="30 Min">30 Min</option>
+                    <option value="45 Min">45 Min</option>
+                    <option value="60 Min">60 Min</option>
+                    <option value="90 Min">90 Min</option>
+                    <option value="120 Min">120 Min</option>
+                </select><br/>
                 <br/>
+
                 <label for="schwierigkeit">Schwierigkeit:</label>
                         <select name="schwierigkeit" id="schwierigkeit">
                             <option value="leicht">leicht</option>
                             <option value="mittel">mittel</option>
                             <option value="schwer">schwer</option>
                             <option value="sehr schwer">sehr schwer</option>
-                        </select></br>
+                        </select><br/>
                 <br/>
+
+                <div class="kategorie">
+                    Kategorie:
+                    <input type="checkbox" id="cat1" name="cat1" value="fleisch" checked="checked">
+                    <label for="cat1"> Fleisch </label>
+                    <input type="checkbox" id="cat2" name="cat2" value="vegetarisch">
+                    <label for="cat2"> Vegetarisch </label><br>
+                    <input type="checkbox" id="cat3" name="cat3" value="vegan">
+                    <label for="cat3"> Vegan </label><br>
+                </div><br/>
+
+
                 <label for="beschreibung">Beschreibung:</label><br/>
-                <input type="text" rows="20" maxlength="2000" name="beschreibung" form="beschreibung" id="beschreibung" placeholder="Inhalt eingeben..." ></br>
+                <textarea class="beschreibung" name="beschreibung" maxlength="500" placeholder="Beschreibung eingeben..."></textarea><br/>
+
                 <label for="zutaten">Zutaten:</label>
                 <table id="zutatenTable">
                     <tr>
@@ -47,34 +124,44 @@
                 </table>
                 <button class="button" id="plus" type="button" onclick="addRow()">+</button>
                 <br/>
+
+                <input type="hidden" id="tableLength" name="tableLength" value="">
+
                 <label for="zubereitung">Zubereitung:</label><br/>
-                <input type="text" rows="20" maxlength="2000" name="zubereitung" form="zubereitung" id="zubereitung" placeholder="Inhalt eingeben..."></br>
+                <textarea class="zubereitung" name="anleitung" placeholder="Zubereitung eingeben..."></textarea><br/>
                 </br>
-                +++ Bilder hochladen +++
-                <button class="button" id="create" type="submit">Erstellen</button>
+
+                <div class="bottom-buttons">
+                    <button type="submit" class="button" id="create" type="submit">Erstellen</button>
+                    <a href="kochbuch.php"><button type="button" class="button" id="cancel">Abbrechen</button></a>
+                </div>
+
+
             </form>
             </br>
 
 
         </div>
 
-        <div id="bottom-buttons">
-            <button class="button" id="cancel">Abbrechen</button>
-        </div>
+
 
     </div>
 </div>
 
 <script>
+    var counter = 1;
     function addRow() {
         var table = document.getElementById("zutatenTable");
         var row = table.insertRow(-1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        cell1.innerHTML = "<input type=\"text\" name=\"menge\" id=\"menge\" size=\"40\" placeholder=\"Menge eingeben...\">";
-        cell2.innerHTML = "<input type=\"text\" name=\"zutaten\" id=\"zutaten\" size=\"40\" placeholder=\"Zutaten eingeben...\">";
+        cell1.innerHTML = '<input type="text" name="menge'+ counter +'" id="menge" size="40" placeholder="Menge eingeben...">';
+        cell2.innerHTML = '<input type="text" name="zutaten'+ counter +'" id="zutaten" size="40" placeholder="Zutaten eingeben...">';
         cell3.innerHTML = "<button class=\"button\" type=\"button\" onclick=\"deleteRow(this)\">-</button>"
+
+        document.getElementById("tableLength").value = counter;
+        counter ++;
     }
 
     function deleteRow(row)
@@ -87,3 +174,13 @@
 
 </body>
 </html>
+    <?php
+    ;
+} else if($sess != true){
+
+    echo"Bitte einloggen!". " ". '<a href="login.php">zum Login</a>';
+    echo'<br>';
+    echo"Noch kein Mitglied?". " ". '<a href="registrieren.php">Mitglied werden!</a>';
+
+}
+?>
