c19cbc57d56e74cbfd7d7f3b6d89c12ce8e31675
Felix Wegener <felix.wegener@iem.thm.de>
2020-07-02 17:11:09 +0200

    RezeptBild geht, aber Bild nicht formatiert

--- externFiles/kochbuch.php
+++ externFiles/kochbuch.php
@@ -80,6 +80,7 @@ if($nutzer==0){
                         $rezeptID= $rezept['rid'];
                         $title = $rezept['titel'];
                         $timestamp = $rezept['cdate'];
+                        $pic = $rezept['pic'];
                         $kategorienListe = $rezept['kategorien'];
                         $dauer = $rezept['dauer'];
                         $schwierigkeit = $rezept['schwierigkeit'];
@@ -89,7 +90,7 @@ if($nutzer==0){
                         echo '<div class="recipe-preview-container">';
                         echo '<div class="recipe-preview">';
                         echo '<div class="recipe-preview-pic">';
-                        echo '<img alt="Rezept-Vorschau" class="recipe-pic" src="">';
+                        echo '<img alt="Rezept-Vorschau" class="recipe-pic" src="../images/rezepte/'.$pic.'">';
                         echo '</div>';
                         echo '<div class="recipe-preview-info">';
                         echo '<div class="kategorien">';
