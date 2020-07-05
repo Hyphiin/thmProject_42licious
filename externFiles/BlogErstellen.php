09ffd6a68285d004b852f7f250674e39f44c1035
Marcel <marcel.sippel@iem.thm.de>
2020-07-05 14:21:30 +0200

    Blog max Länge 3000

--- externFiles/BlogErstellen.php
+++ externFiles/BlogErstellen.php
@@ -53,7 +53,7 @@ if ($sess == true) {
                 <input type="text" name="titel" size="40" maxlength="255">
                 <br><br>
                 Inhalt:<br>
-                <textarea name="inhalt" maxlength="255"></textarea>
+                <textarea name="inhalt" maxlength="3000"></textarea>
                 <br><br>
                 <input type="submit" value="Bloggen" class="button">
             </form>
