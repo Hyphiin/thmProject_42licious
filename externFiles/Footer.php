e1d53946ed62c7e82bad1dc5ae4d18e397fe14a5
Felix Wegener <felix.wegener@iem.thm.de>
2020-07-06 12:37:15 +0200

    Impressum hinzugefügt

--- externFiles/Footer.php
+++ externFiles/Footer.php
@@ -4,7 +4,7 @@
         <div>SWEP WS19/20</div>
     </div>
     <div class="footertable">
-        <div>Impressum</div>
+        <a id="Impressum" href="Impressum.html"><div>Impressum</div></a>
         <div>Datenschutz</div>
     </div>
     <div class="footertable">
